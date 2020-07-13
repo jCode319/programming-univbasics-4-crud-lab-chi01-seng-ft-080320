49addea79c3c01eca5092e82cd1f6772936dc31c
Jarrell Jackson <jdjack3@gmail.com>
2020-07-13 20:58:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,9 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  add_element_to_end_of_array.push("array", "element")
-  p add_element_to_end_of_array
+  array = ["wow", "I", "am", "really", "learning"]
+  element = "arrays!"
+  add_element_to_end_of_array.push("element")
 end
 
 def add_element_to_start_of_array(array, element)
