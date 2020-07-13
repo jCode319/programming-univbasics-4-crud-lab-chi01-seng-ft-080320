3060114417363c3306edd257b1f6266f1341562a
Jarrell Jackson <jdjack3@gmail.com>
2020-07-13 21:21:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,11 +15,11 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  element.pop
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  element.shift
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
